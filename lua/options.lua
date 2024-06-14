46a804a9acd2eb3290715f063d008f77489047cc
Tan Nguyen <tantit91@gmail.com>
2024-06-14 07:23:52 +0200

    add code formatter

--- lua/options.lua
+++ lua/options.lua
@@ -1,25 +1,48 @@
-vim.g.mapleader = ','
-vim.g.maplocalleader = ' '
+vim.g.mapleader = ","
+vim.g.maplocalleader = " "
 
-vim.o.clipboard = 'unnamedplus'
-
-vim.o.number = true
--- vim.o.relativenumber = true
-
-vim.o.signcolumn = 'yes'
-
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-
+vim.o.clipboard = "unnamedplus"
+vim.o.signcolumn = "yes"
 vim.o.updatetime = 300
-
 vim.o.termguicolors = true
-
-vim.o.mouse = 'a'
-
--- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
--- optionally enable 24-bit colour
-vim.opt.termguicolors = true
+vim.o.mouse = "a"
+vim.o.autoread = true
+vim.o.autowrite = true
+vim.o.backspace = "indent,eol,start"
+vim.o.cursorline = true
+vim.o.encoding = "utf-8"
+vim.o.expandtab = true
+vim.o.softtabstop = 0
+vim.o.clipboard = "unnamedplus"
+vim.o.foldlevel = 20
+vim.o.foldmethod = "syntax"
+vim.o.hidden = true
+vim.o.history = 50
+vim.o.hlsearch = true
+vim.o.ignorecase = true
+vim.o.incsearch = true
+vim.o.laststatus = 2
+vim.o.linebreak = true
+vim.o.list = true
+vim.o.modelines = 1
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.ruler = true
+vim.o.scrolljump = 3
+vim.o.scrolloff = 5
+vim.o.shiftwidth = 4
+vim.o.showcmd = true
+vim.o.smartcase = true
+vim.o.splitbelow = true
+vim.o.splitright = true
+vim.o.tabstop = 4
+vim.o.textwidth = 78
+vim.o.timeoutlen = 600
+vim.o.ttimeoutlen = 50
+vim.o.visualbell = true
+vim.o.wildmenu = true
+vim.o.winheight = 24
+vim.o.winwidth = 85
+vim.o.wrap = true
+vim.o.wrapmargin = 2
+vim.o.diffopt = "vertical"
--- lua/pluginlist.lua
+++ lua/pluginlist.lua
@@ -1,174 +1,284 @@
 return {
+	{
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup()
+		end,
+	},
 
-    {
-        "numToStr/Comment.nvim",
-        config = function()
-            require("Comment").setup()
-        end
-    },
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+		config = function()
+			vim.cmd("colorscheme gruvbox")
+		end,
+	},
 
-    {
-        "ellisonleao/gruvbox.nvim",
-        priority = 1000,
-        config = function()
-            vim.cmd("colorscheme gruvbox")
-        end
-    },
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
+	"vim-test/vim-test",
 
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-    "vim-test/vim-test",
+	{
+		"junnplus/lsp-setup.nvim",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"williamboman/mason.nvim", -- optional
+			"williamboman/mason-lspconfig.nvim", -- optional
+		},
+		config = function()
+			require("lsp-setup").setup({
+				servers = {
+					jedi_language_server = {},
+					clangd = {},
+				},
+			})
+		end,
+	},
 
 	{
 		"preservim/vimux",
-        keys = {
-            { '<leader>vp', '<cmd>VimuxPromptCommand<cr>' },
-            { '<leader>vl', '<cmd>VimuxRunLastCommand<cr>' },
-            { '<leader>vi', '<cmd>VimuxInspectRunner<cr>' },
-            { '<leader>vq', '<cmd>VimuxCloseRunner<cr>' },
-            { '<leader>vs', '<cmd>VimuxInterruptRunner<cr>' },
-            { '<leader>vz', '<cmd>VimuxZoomRunner<cr>' },
-        }
-    },
+		keys = {
+			{ "<leader>vp", "<cmd>VimuxPromptCommand<cr>" },
+			{ "<leader>vl", "<cmd>VimuxRunLastCommand<cr>" },
+			{ "<leader>vi", "<cmd>VimuxInspectRunner<cr>" },
+			{ "<leader>vq", "<cmd>VimuxCloseRunner<cr>" },
+			{ "<leader>vs", "<cmd>VimuxInterruptRunner<cr>" },
+			{ "<leader>vz", "<cmd>VimuxZoomRunner<cr>" },
+		},
+	},
 
-    {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'make',
-    },
+	{
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = "make",
+	},
 
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("lualine").setup({
+				options = {
+					icons_enabled = true,
+					theme = "codedark",
+					section_separators = { left = "", right = "" },
+					component_separators = { left = "", right = "" },
+					disabled_filetypes = {
+						statusline = {},
+						winbar = {},
+					},
+					ignore_focus = {},
+					always_divide_middle = true,
+					globalstatus = false,
+					refresh = {
+						statusline = 1000,
+						tabline = 1000,
+						winbar = 1000,
+					},
+				},
+			})
+		end,
+	},
 
-    {
-        'nvim-lualine/lualine.nvim',
-        dependencies = {
-            "nvim-tree/nvim-web-devicons"
-        },
-        config = function()
-            require("lualine").setup({
-                options = {
-                    icons_enabled = true,
-                    theme = 'codedark',
-                    section_separators = { left = '', right = '' },
-                    component_separators = { left = '', right = '' },
-                    disabled_filetypes = {
-                      statusline = {},
-                      winbar = {},
-                    },
-                    ignore_focus = {},
-                    always_divide_middle = true,
-                    globalstatus = false,
-                    refresh = {
-                      statusline = 1000,
-                      tabline = 1000,
-                      winbar = 1000,
-                    }
-                  },
-            })
-        end,
-    },
+	"folke/neodev.nvim", -- new
 
-    'folke/neodev.nvim', -- new
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+		},
+		config = function()
+			local cmp = require("cmp")
+			cmp.setup({
+				snippet = {
+					-- REQUIRED - you must specify a snippet engine
+					expand = function(args)
+						vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+						-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+						-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+						-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+						-- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+					end,
+				},
+				window = {
+					-- completion = cmp.config.window.bordered(),
+					-- documentation = cmp.config.window.bordered(),
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "vsnip" }, -- For vsnip users.
+					-- { name = 'luasnip' }, -- For luasnip users.
+					-- { name = 'ultisnips' }, -- For ultisnips users.
+					-- { name = 'snippy' }, -- For snippy users.
+				}, {
+					{ name = "buffer" },
+				}),
+			})
 
-    {
-        'hrsh7th/nvim-cmp',
-        dependencies = {
-            'L3MON4D3/LuaSnip',
-            'saadparwaiz1/cmp_luasnip',
-            'rafamadriz/friendly-snippets',
-            'hrsh7th/cmp-nvim-lsp',
-        },
-    },
+			-- Set up lspconfig.
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+			require("lspconfig")["jedi_language_server"].setup({
+				capabilities = capabilities,
+			})
+		end,
+	},
 
-    {
-        'nvim-treesitter/nvim-treesitter',
-        build = ':TSUpdate',
-    },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+	},
 
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		keys = {
+			{ "<leader>f", "<cmd>Telescope find_files<cr>" },
+			{ "<leader>b", "<cmd>Telescope buffers<cr>" },
+		},
+	},
 
-    { 
-        'nvim-telescope/telescope.nvim',
-        dependencies = { 'nvim-lua/plenary.nvim' },
-        keys = {
-            { '<leader>f', '<cmd>Telescope find_files<cr>' },
-            { '<leader>b', '<cmd>Telescope buffers<cr>' },
-        }
-    },
+	{
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = "make",
+	},
 
-    {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'make',
-    },
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("lualine").setup({
+				options = {
+					icons_enabled = true,
+					theme = "codedark",
+					section_separators = { left = "", right = "" },
+					component_separators = { left = "", right = "" },
+					disabled_filetypes = {
+						statusline = {},
+						winbar = {},
+					},
+					ignore_focus = {},
+					always_divide_middle = true,
+					globalstatus = false,
+					refresh = {
+						statusline = 1000,
+						tabline = 1000,
+						winbar = 1000,
+					},
+				},
+			})
+		end,
+	},
 
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		},
+		keys = {
+			{ "<C-n>", "<cmd>Neotree toggle<cr>" },
+		},
+		config = function()
+			require("neo-tree").setup({
+				close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
+				popup_border_style = "rounded",
+				enable_git_status = true,
+				enable_diagnostics = true,
+				window = {
+					position = "right",
+					width = 40,
+					mapping_options = {
+						noremap = true,
+						nowait = true,
+					},
+				},
+			})
+		end,
+	},
 
-    {
-        'nvim-lualine/lualine.nvim',
-        dependencies = {
-            "nvim-tree/nvim-web-devicons"
-        },
-        config = function()
-            require("lualine").setup({
-                options = {
-                    icons_enabled = true,
-                    theme = 'codedark',
-                    section_separators = { left = '', right = '' },
-                    component_separators = { left = '', right = '' },
-                    disabled_filetypes = {
-                      statusline = {},
-                      winbar = {},
-                    },
-                    ignore_focus = {},
-                    always_divide_middle = true,
-                    globalstatus = false,
-                    refresh = {
-                      statusline = 1000,
-                      tabline = 1000,
-                      winbar = 1000,
-                    }
-                  },
-            })
-        end,
-    },
+	{
+		"aserowy/tmux.nvim",
+		config = function()
+			return require("tmux").setup()
+		end,
+	},
 
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        dependencies = {
-          "nvim-lua/plenary.nvim",
-          "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-          "MunifTanjim/nui.nvim",
-          -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-        },
-        config = function ()
-          require("neo-tree").setup({
-            close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
-            popup_border_style = "rounded",
-            enable_git_status = true,
-            enable_diagnostics = true,
-            window = {
-              position = "right",
-              width = 40,
-              mapping_options = {
-                noremap = true,
-                nowait = true,
-              },
-            },
-          })
-        end,
-    },
+	{
+		"ThePrimeagen/refactoring.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("refactoring").setup()
+		end,
+	},
 
-    {
-        "aserowy/tmux.nvim",
-        config = function() return require("tmux").setup() end
-    },
+	{
+		"stevearc/conform.nvim",
+		config = function()
+			require("conform").setup({
+				-- Map of filetype to formatters
+				formatters_by_ft = {
+					lua = { "stylua" },
+					-- Conform will run multiple formatters sequentially
+					go = { "goimports", "gofmt" },
+					-- Use a sub-list to run only the first available formatter
+					javascript = { { "prettierd", "prettier" } },
+					-- You can use a function here to determine the formatters dynamically
+					python = { "isort", "black" },
+					-- Use the "*" filetype to run formatters on all filetypes.
+					["*"] = { "codespell" },
+					-- Use the "_" filetype to run formatters on filetypes that don't
+					-- have other formatters configured.
+					["_"] = { "trim_whitespace" },
+				},
+				-- If this is set, Conform will run the formatter on save.
+				-- It will pass the table to conform.format().
+				-- This can also be a function that returns the table.
+				format_on_save = {
+					-- I recommend these options. See :help conform.format for details.
+					lsp_fallback = true,
+					timeout_ms = 500,
+				},
+				-- If this is set, Conform will run the formatter asynchronously after save.
+				-- It will pass the table to conform.format().
+				-- This can also be a function that returns the table.
+				format_after_save = {
+					lsp_fallback = true,
+				},
+				-- Set the log level. Use `:ConformInfo` to see the location of the log file.
+				log_level = vim.log.levels.ERROR,
+				-- Conform will notify you when a formatter errors
+				notify_on_error = true,
+			})
 
-    {
-      "ThePrimeagen/refactoring.nvim",
-      dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-treesitter/nvim-treesitter",
-      },
-      config = function()
-        require("refactoring").setup()
-      end,
-    },
+			-- You can set formatters_by_ft and formatters directly
+			require("conform").formatters_by_ft.lua = { "stylua" }
+			require("conform").formatters.my_formatter = {
+				command = "my_cmd",
+			}
+		end,
+	},
 }
